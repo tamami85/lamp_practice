b971d1b92b00c1633fa6245631b35df44ced51f1
tama <tamami.hmm@gmail.com>
2021-05-24 15:25:17 +0900

    課題２のクエリを作り直して、どっかで使いそうなコードを作りました。その２

--- www/model/order_details.php
+++ www/model/order_details.php
@@ -49,22 +49,49 @@ function get_order_details($db, $history_id){
 function get_total_price($db, $history_id){
     $sql = "
             SELECT
-                history_id AS 注文番号,
-                SUM(price) AS 該当の注文の合計金額
+                SUM(price) AS total_price,
+                order_details.history_id AS history_number,
+                purchase_history.created,
             FROM
                 order_details
+            LEFT OUTER JOIN
+                purchase_history
+            ON
+                order_details.history_id = purchase_history.history_id
             GROUP BY
                 history_id = ?              
             ";
     $params = array($history_id);
     return fetch_query($db, $sql, $params);
 }
+
+
+//管理者用の配列取得
+function admin_get_total_price($db, $history_id){
+    $sql = "
+            SELECT
+                SUM(price) AS total_price,
+                order_details.history_id AS history_number,
+                purchase_history.created,
+            FROM
+                order_details
+            LEFT OUTER JOIN
+                purchase_history
+            ON
+                order_details.history_id = purchase_history.history_id
+            GROUP BY
+                history_id              
+            ";
+    return fetch_all_query($db, $sql);
+}
+
     
 
 
 
 
 // どこに置くのか後で考えまひょ
+
 
 // 明細の一覧表示(表示項目は「商品名」「購入時の商品価格」「購入数」「小計」とする。)
 $order_details = get_order_details($db, $history_id);//配列で明細を一気に取得してる
@@ -73,8 +100,21 @@ foreach($order_details as $value){
     $value['price'];//「購入時の商品価格」 
     $value['amount'];//「購入数」
 }
-// 明細の小計、および画面上部に表示させる「合計金額」
-$subtotal_price['該当の注文の合計金額'] = get_total_price($db, $history_id);//配列で１行取得したから、'該当の注文の合計金額'カラムの金額だけを抽出
 
+//小計の表示
+$total_price['total_price'] = get_total_price($db, $history_id);//配列で１行取得したから、'該当の注文の合計金額'カラムの金額だけを抽出
 
-    
+// 表示項目は「注文番号」「購入日時」「該当の注文の合計金額」とする。
+foreach($total_price as $value){
+    $value['history_number'];//「注文番号」
+    $value['created'];//「購入日時」 
+    $value['total_price'];//「該当の注文の合計金額」
+}
+
+// 表示項目は「注文番号」「購入日時」「該当の注文の合計金額」とする。（管理者用）
+$admin_total_price = admin_get_total_price($db, $history_id);
+foreach($admin_total_price as $value){
+    $value['history_number'];//「注文番号」
+    $value['created'];//「購入日時」 
+    $value['total_price'];//「該当の注文の合計金額」
+}
